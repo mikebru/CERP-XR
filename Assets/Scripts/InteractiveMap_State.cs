9b6fff474e0f6e21b88a8470f1ae0e88c6454af6
mikebru <michael.bruner3@gmail.com>
2019-10-23 00:18:51 -0500

    Initial commit

---
+++ Assets/Scripts/InteractiveMap_State.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveMap_State : MonoBehaviour
+{
+    public bool[] BuoyData;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SetBuoy(float placedBuoy)
+    {
+        int buoyCout = (int)Mathf.Abs(placedBuoy);
+
+        if(placedBuoy == 0)
+        {
+            for(int i=0; i< BuoyData.Length; i++)
+            {
+                BuoyData[i] = false;
+            }
+        }
+
+        if(placedBuoy > 0)
+        {
+            BuoyData[buoyCout] = true;
+        }
+        else
+        {
+            BuoyData[buoyCout] = false;
+        }
+    }
+
+}
