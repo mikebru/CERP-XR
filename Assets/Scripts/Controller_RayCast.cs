eb5df196803d4b218ad15f56296ae45182e90e19
mikebru <32722658+mikebru@users.noreply.github.com>
2019-10-28 20:01:08 -0500

    updates to interaction, UI cursor

---
+++ Assets/Scripts/Controller_RayCast.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_RayCast : MonoBehaviour
+{
+
+    Camera local_cam;
+
+    public GameObject[] Walls;
+    public Camera[] CaptureCameras;
+
+    public InteractiveUI currentUI;
+
+    public GameObject cursor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        local_cam = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(currentUI != null)
+            {
+                currentUI.OnClick();
+            }
+        }
+      
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                Vector3 hitPosition = hit.point - hit.transform.position;
+                //change axis
+                if (hitPosition.x <= .025f && hitPosition.x >= -.025f)
+                {
+                    //Debug.Log("here");
+                    hitPosition = new Vector3(hitPosition.z, hitPosition.y, 0);
+                }
+                //Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, 10);
+
+                //re-project cast
+                for (int i = 0; i < Walls.Length; i++)
+                {
+                    if (hit.collider.gameObject == Walls[i])
+                    {
+                        RaycastHit cam_hit;
+
+                        Vector3 origin = CaptureCameras[i].transform.position + hitPosition;
+
+                        if (Physics.Raycast(origin, CaptureCameras[i].transform.forward, out cam_hit))
+                        {
+                        //Debug.DrawRay(origin, CaptureCameras[i].transform.forward * cam_hit.distance, Color.green, 10);
+
+                            cursor.transform.position = cam_hit.point - new Vector3(0,0,.1f);
+
+                            if(cam_hit.collider.gameObject.GetComponent<InteractiveUI>() != null && currentUI != cam_hit.collider.gameObject.GetComponent<InteractiveUI>())
+                            {
+                                if (currentUI != null)
+                                {
+                                    currentUI.OnHoverExit();
+                                }
+
+                                currentUI = cam_hit.collider.gameObject.GetComponent<InteractiveUI>();
+                                currentUI.OnHoverEnter();
+                            }
+                            else if(currentUI != null && cam_hit.collider.gameObject.GetComponent<InteractiveUI>() == null)
+                            {
+                                currentUI.OnHoverExit();
+                                currentUI = null;
+                            }
+
+                        }
+                        else if(currentUI != null)
+                        {
+                            currentUI.OnHoverExit();
+                            currentUI = null;
+                        }
+                    }
+                }
+             }
+            else if (currentUI != null)
+            {
+                currentUI.OnHoverExit();
+                currentUI = null;
+            }
+
+
+
+    }
+}
