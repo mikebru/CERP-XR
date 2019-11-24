5697626d0d1606727d7d697bfbe95be4de6da1c9
mikebru <michael.bruner3@gmail.com>
2019-11-24 12:34:52 -0600

    added in animated hurricane and controller instruction video

---
+++ Assets/Scripts/HurricaneStates.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurricaneStates : MonoBehaviour
+{
+    public Material hurricane_Mat;
+    public Texture2D[] masks;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void setMask(int index)
+    {
+
+        hurricane_Mat.SetTexture("_maskTexture", masks[index]);
+
+    }
+
+
+}
